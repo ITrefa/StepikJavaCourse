dc72853b731b09d33d345a81986faa3d0c0af7bb
ITrefa <katilaletina113@gmail.com>
2020-03-02 18:22:46 +0300

    implement tasks from I/O part

--- src/InputOutput/Symbols/Main.java
+++ src/InputOutput/Symbols/Main.java
@@ -4,8 +4,28 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.Scanner;
+
+
+/*
+Напишите программу, читающую текст из System.in и выводящую в System.out сумму всех встреченных в тексте вещественных чисел
+с точностью до шестого знака после запятой. Числом считается последовательность символов,
+отделенная от окружающего текста пробелами или переводами строк и успешно разбираемая методом Double.parseDouble.
+*/
 
 public class Main {
+
+    public static void main(String[] args) {
+        double sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            if (scanner.hasNextDouble()) {
+                sum += scanner.nextDouble();
+            } else scanner.next();
+        }
+        System.out.format("%.6f", sum);
+    }
+
 
     /*
     Реализуйте метод, который зачитает данные из InputStream и преобразует их в строку, используя заданную кодировку.
