a2b6e4933371ad323237cd23c3a780d799d873c0
ITrefa <katilaletina113@gmail.com>
2020-03-05 15:11:16 +0300

    implement AsciiCharSequence task

--- src/Objects/Abstract/AsciiCharSequence.java
+++ src/Objects/Abstract/AsciiCharSequence.java
@@ -1,4 +1,5 @@
 package Objects.Abstract;
+
 /*
 Напишите класс AsciiCharSequence, реализующий компактное хранение последовательности ASCII-символов (их коды влезают в один байт) в массиве байт.
 По сравнению с классом String, хранящим каждый символ как char, AsciiCharSequence будет занимать в два раза меньше памяти.
@@ -11,6 +12,36 @@ package Objects.Abstract;
 Сигнатуры методов и ожидания по их поведению смотрите в описании интерфейса java.lang.CharSequence (JavaDoc или исходники).
 В данном задании методам charAt() и subSequence() всегда будут подаваться корректные входные параметры, поэтому их проверкой и обработкой ошибок заниматься не нужно. Тем более мы еще не проходили исключения.
  */
-public class AsciiCharSequence /* extends/implements */ {
-    //implementation
+public class AsciiCharSequence implements CharSequence {
+    private byte[] content;
+
+    public AsciiCharSequence(byte[] content) {
+        this.content = content;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return (char) content[index];
+    }
+
+    @Override
+    public int length() {
+        return content.length;
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        byte[] result = new byte[end - start];
+        if (end - start >= 0) System.arraycopy(content, start, result, 0, end - start);
+        return new AsciiCharSequence(result);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder(content.length);
+        for (byte b : content) {
+            result.append((char) b);
+        }
+        return result.toString();
+    }
 }
